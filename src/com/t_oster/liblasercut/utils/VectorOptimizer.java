d81f7fd46cfe3bc89a3e672a28046292902510a9
Thomas Oster <thomas.oster@rwth-aachen.de>
2012-12-05 18:52:22 +0100

    Fix: ArrayIndexOutOfBoundsException. Fix: Optimizer compared End with End instead of End with Start Improved: Optimizer can invert element if faster

--- src/com/t_oster/liblasercut/utils/VectorOptimizer.java
+++ src/com/t_oster/liblasercut/utils/VectorOptimizer.java
@@ -19,25 +19,39 @@ public class VectorOptimizer
     //INNER_FIRST,
     NEAREST
   }
-  
+
   class Element
   {
     LaserProperty prop;
     Point start;
     List<Point> moves = new LinkedList<Point>();
+    void invert()
+    {
+      if (!moves.isEmpty())
+      {
+        moves.add(0, start);
+        start = moves.remove(moves.size()-1);
+        List<Point> inv = new LinkedList<Point>();
+        while (!moves.isEmpty())
+        {
+          inv.add(moves.remove(moves.size()-1));
+        }
+        moves = inv;
+      }
+    }
     Point getEnd()
     {
       return moves.isEmpty() ? start : moves.get(moves.size()-1);
     }
   }
-  
+
   private OrderStrategy strategy = OrderStrategy.FILE;
-  
+
   public VectorOptimizer(OrderStrategy s)
   {
     this.strategy = s;
   }
-  
+
   private List<Element> divide(VectorPart vp)
   {
     List<Element> result = new LinkedList<Element>();
@@ -85,15 +99,19 @@ public class VectorOptimizer
     }
     return result;
   }
-  
+
   private double dist(Point a, Point b)
   {
     return Math.sqrt((a.y-b.y)*(a.y-b.y)+(a.x-b.x)*(a.x-b.x));
   }
-  
+
   private List<Element> sort(List<Element> e)
   {
     List<Element> result = new LinkedList<Element>();
+    if (e.isEmpty())
+    {
+      return result;
+    }
     switch (strategy)
     {
       case FILE:
@@ -107,32 +125,56 @@ public class VectorOptimizer
         while (!e.isEmpty())
         {
           Point end = result.get(result.size()-1).getEnd();
-          //find next
+          //find nearest element
           int next = 0;
+          //invert element direction if endpoint is nearer
+          boolean invert = false;
           double dst = -1;
           for (int i = 1; i < e.size(); i++)
           {
-            double nd = dist(e.get(i).getEnd(), end);
+            //check distance to startpoint
+            double nd = dist(e.get(i).start, end);
             if (nd < dst || dst == -1)
             {
               next = i;
               dst = nd;
+              invert = false;
+            }
+            if (!e.get(i).start.equals(e.get(i).getEnd()))
+            {
+              //check distance to endpoint
+              nd = dist(e.get(i).getEnd(), end);
+              if (nd < dst || dst == -1)
+              {
+                next = i;
+                dst = nd;
+                invert = true;
+              }
             }
           }
           //add next
-          result.add(e.remove(next));
+          if (invert)
+          {
+            Element m = e.remove(next);
+            m.invert();
+            result.add(m);
+          }
+          else
+          {
+            result.add(e.remove(next));
+          }
         }
         break;
       }
     }
     return result;
   }
-  
+
   public VectorPart optimize(VectorPart vp)
   {
     List<Element> opt = this.sort(this.divide(vp));
-    LaserProperty cp = opt.get(0).prop;
-    VectorPart result = new VectorPart(opt.get(0).prop, vp.getDPI());
+    LaserProperty cp = opt.isEmpty() ? vp.getCurrentCuttingProperty() : opt.get(0).prop;
+    VectorPart result = new VectorPart(cp, vp.getDPI());
     for (Element e : opt)
     {
       if (!e.prop.equals(cp))
@@ -148,5 +190,5 @@ public class VectorOptimizer
     }
     return result;
   }
-  
+
 }
