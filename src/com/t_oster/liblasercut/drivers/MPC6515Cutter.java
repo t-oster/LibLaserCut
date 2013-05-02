47fdb3032fc5245854294f621cb7516d6432feb9
Matthias Melcher <git@matthiasm.com>
2013-05-02 18:10:58 +0200

    Cleaning up more and more. Added todo list and todo entries. Still trouble finding the correct origin. Laser output flipped vertically.

--- src/com/t_oster/liblasercut/drivers/MPC6515Cutter.java
+++ src/com/t_oster/liblasercut/drivers/MPC6515Cutter.java
@@ -16,6 +16,31 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with LibLaserCut.  If not, see <http://www.gnu.org/licenses/>.
  **/
+
+/*
+ * TODO:
+ * - check if x and/or y coordinates must be flipped
+ * - after the previous point, make sure that the bounding boxes correspond to the new coordinates
+ * - the origin is wrong! How does it relate to setting the machines origin?
+ * - VERIFY: allow speed change at any time
+ * - VERIFY: allow power setting change at any time
+ * - VERIFY: additional parameter for "corner power"
+ * - remove unused functions
+ * - rename remaining functions if generic
+ * - option for MOL filename
+ * - connect to the USB uploader (option for executable path)
+ * - read settings from machine and update configuration
+ * - machine photo
+ * - line offset for cutting!
+ * - what is the use of the USB camera option?
+ * - part distribution
+ * - optional machine margin
+ * - optional preferred material position
+ * - test for Z control, z endstop?
+ * - test remaining parameters
+ * - convert position information to integer format throughout to keep 100% precision in relative movements
+ */
+
 package com.t_oster.liblasercut.drivers;
 
 import com.apple.crypto.provider.Debug;
@@ -28,11 +53,11 @@ import com.t_oster.liblasercut.ProgressListener;
 import com.t_oster.liblasercut.Raster3dPart;
 import com.t_oster.liblasercut.RasterPart;
 import com.t_oster.liblasercut.VectorCommand;
-import com.t_oster.liblasercut.VectorCommand.CmdType;
+import static com.t_oster.liblasercut.VectorCommand.CmdType.LINETO;
+import static com.t_oster.liblasercut.VectorCommand.CmdType.MOVETO;
+import static com.t_oster.liblasercut.VectorCommand.CmdType.SETPROPERTY;
 import com.t_oster.liblasercut.VectorPart;
 import com.t_oster.liblasercut.platform.Util;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -49,19 +74,25 @@ public class MPC6515Cutter extends LaserCutter
   private static Double[] SUPPORTED_RESOLUTIONS = new Double[]{200d,500d,1000d};
   private static final String BED_WIDTH = "bed width";
   private static final String BED_HEIGHT = "bed height";
+  private static final String CORNER_POWER_FACTOR = "corner power factor";
   private static String[] PROPERTIES = new String[]{
     BED_WIDTH,
     BED_HEIGHT,
+    CORNER_POWER_FACTOR,
   };
  
-  private double bedWidth = 300;
-  private double bedHeight = 210;
+  private double bedWidth = 900;
+  private double bedHeight = 600;
+  private double cornerPowerFactor = 0.8;
+
+  private double currentPower = 40;
+  private double currentSpeed = 80;
   
   private double firstX = 0.0, firstY = 0.0, prevX = 0.0, prevY = 0.0;
   private double bboxMinX = 0.0, bboxMinY = 0.0, bboxMaxX = 0.0, bboxMaxY = 0.0, bboxWidth, bboxHeight;
   boolean firstVector;
   
-  private int motionBlockStart;
+  private int motionBlockStart = -1;
   private int motionBlockSize;
   private int nMotionBlocks;
   private int nMoveRelative;
@@ -104,6 +135,11 @@ public class MPC6515Cutter extends LaserCutter
     out.seek(motionBlockStart-8);
     molCmd(out, 0x46, 0x09, 0x00, 0x80,  (n-motionBlockStart)/4);
     out.seek(n);    
+  }
+  
+  private boolean molInMotionBlock()
+  {
+    return (motionBlockStart!=-1);
   }
 
   private void molCmdTestMotionBlock(RandomAccessFile out, int nWords) throws IOException
@@ -463,7 +499,41 @@ public class MPC6515Cutter extends LaserCutter
       }
     }    
   }
+
+  private void molSetPowerAndSpeed(RandomAccessFile out, double cornerPower, double maxPower, double startSpeed, double maxSpeed) throws IOException
+  {
+  // Laser Power
+    molWriteBytes(out, 0x46, 0x0e, 0x00, 0x05);
+    molWriteInt(out, (int)(cornerPower*100));  // (40.000000%) corner power
+    molWriteInt(out, (int)(maxPower*100));  // (40.000000%) maximum power
+    molWriteFloat(out, (float)(startSpeed*208.33)); // (5%) cutting start speed
+    molWriteFloat(out, (float)(maxSpeed*208.33)); // (100%) maximum cutting speed
+    molWriteInt(out, 0);  // unknown
+  // Laser Power Long
+    molWriteBytes(out, 0x46, 0x0e, 0x00, 0x07);
+    molWriteInt(out, (int)(cornerPower*100));  // (40.000000%) corner power
+    molWriteInt(out, (int)(maxPower*100));  // (40.000000%) maximum power
+    molWriteInt(out, (int)(cornerPower*100));  // (40.000000%) corner power (second head?)
+    molWriteInt(out, (int)(maxPower*100));  // (40.000000%) maximum power (second head?)
+    molWriteFloat(out, (float)(startSpeed*208.33)); // (5%) cutting start speed
+    molWriteFloat(out, (float)(maxSpeed*208.33)); // (100%) maximum cutting speed
+    molWriteInt(out, 0);  // unknown
+  }
   
+  private void molChangePowerAndSpeed(RandomAccessFile out, double cornerPower, double maxPower, double startSpeed, double maxSpeed) throws IOException
+  {
+    boolean inBlock = molInMotionBlock();
+    if (inBlock) {
+      molCmdLaserOff(out);
+      molCmdBlowerOff(out);
+      molCmdEndMotionBlock(out);
+    }
+    molSetPowerAndSpeed(out, cornerPower, maxPower, startSpeed, maxSpeed);
+    if (inBlock) {
+      molCmdBeginMotionBlock(out);
+    }
+  }
+
   // 
   // .MOL file header
   // 
@@ -479,7 +549,7 @@ public class MPC6515Cutter extends LaserCutter
     molWriteInt(out, 1);  // 000c: ?
     //molWriteInt(out, 41);  // 0010: somehow related to the file size or the number of commands
     molWriteInt(out, 2000);  // 0010: somehow related to the file size or the number of commands
-    molWriteInt(out, 1);  // 0014: ?
+    molWriteInt(out, 1);  // 0014: ? also 257 (bitfield?)
     molWriteInt(out, 0);  // 0018: ?
     molWriteInt(out, 0);  // 001c: ?
     molWriteInt(out, (int)(bboxWidth*-208.33));   // 0020: artwork width in steps
@@ -502,10 +572,10 @@ public class MPC6515Cutter extends LaserCutter
     molWriteInt(out, 0);  // 0064: 0
     molWriteInt(out, 0);  // 0068: 0
     molWriteInt(out, 0);  // 006c: 0
-    molWriteInt(out, 1);  // 0070: ?
-    molWriteInt(out, 2);  // 0074: ?
-    molWriteInt(out, 3);  // 0078: ?
-    molWriteInt(out, 5);  // 007c: ?
+    molWriteInt(out, 1);  // 0070: block number of 200 block (home position and start vector)
+    molWriteInt(out, 2);  // 0074: block number of 400 block (test outline)
+    molWriteInt(out, 3);  // 0078: block number of 600 block (cut outline)
+    molWriteInt(out, 5);  // 007c: block number of A00 block (cut artwork)
   }
 
   // 
@@ -571,8 +641,11 @@ public class MPC6515Cutter extends LaserCutter
   // 0264: Artwork Origin
     molWriteBytes(out, 0x40, 0x60, 0x04, 0x03);
     molWriteBytes(out, 0x04, 0x03, 0x00, 0x00);  // Select motion axes
-    molWriteMm(out, (float)(0.5*(bedWidth+bboxWidth))); // (500.005mm) (table width + artwork width) / 2
-    molWriteMm(out, (float)(0.5*(bedHeight+bboxHeight))); // (350.002mm) (table height + artwork height) / 2
+    // Verify setting for origin andsize, coordinate flipping, etc.
+    //molWriteMm(out, (float)(0.5*(bedWidth+bboxWidth))); // FIXME: (500.005mm) (table width + artwork width) / 2
+    //molWriteMm(out, (float)(0.5*(bedHeight+bboxHeight))); // FIXME: (350.002mm) (table height + artwork height) / 2
+    molWriteMm(out, (float)200); // FIXME: (500.005mm) (table width + artwork width) / 2
+    molWriteMm(out, (float)200); // FIXME: (350.002mm) (table height + artwork height) / 2
     nMoveRelative++;
   // 0274: Set Speed
     molWriteBytes(out, 0x41, 0x03, 0x00, 0x03);
@@ -580,10 +653,11 @@ public class MPC6515Cutter extends LaserCutter
     molWriteFloat(out, 41666f); // (200%) max
     molWriteFloat(out, 145833f); // (700%) accel
   // 0284: Start Corner
+    // TODO: Verify setting for origin and size, coordinate flipping, etc.
     molWriteBytes(out, 0x40, 0x60, 0x02, 0x03);
     molWriteBytes(out, 0x04, 0x03, 0x00, 0x00);  // Select motion axes
-    molWriteMm(out, (float)(-bboxWidth)); // (-100mm) left
-    molWriteMm(out, (float)(-bboxHeight)); // (-100mm) bottom
+    molWriteMm(out, (float)(-bboxWidth)); // FIXME: (-100mm) left
+    molWriteMm(out, (float)(-bboxHeight)); // FIXME: (-100mm) bottom
   // 0294: Unknown Command 10
     molWriteBytes(out, 0x48, 0x00, 0x50, 0x80);
     molWriteInt(out, 3);  // Length = 3 words
@@ -621,10 +695,11 @@ public class MPC6515Cutter extends LaserCutter
   // 0428: Begin Motion Block
     molCmdBeginMotionBlock(out);
   // motion  
-    molMoveRel(out, 1041f, 41666f, 1473.12f, 145833f, -bboxWidth*208.33, 0f); // (5%, 200%, 7%, 700%, -100mm, 0mm [rcr])
-    molMoveRel(out, 1473.12f, 41666f, 1473.12f, 145833f, 0f, -bboxHeight*208.33); // (7%, 200%, 7%, 700%, 0mm, -100mm [rcr])
-    molMoveRel(out, 1473.12f, 41666f, 1473.12f, 145833f, bboxWidth*208.33, 0f); // (7%, 200%, 7%, 700%, 100mm, 0mm [rcr])
-    molMoveRel(out, 1473.12f, 41666f, 1041f, 145833f, 0f, bboxHeight*208.33); // (7%, 200%, 5%, 700%, 0mm, 100mm [rcr])
+    // TODO: verify that the correct origin is used, verify if correct after flipping coordinates
+    molMoveRel(out, 1041f, 41666f, 1473.12f, 145833f, -bboxWidth*208.33, 0f); // FIXME: (5%, 200%, 7%, 700%, -100mm, 0mm [rcr])
+    molMoveRel(out, 1473.12f, 41666f, 1473.12f, 145833f, 0f, -bboxHeight*208.33); // FIXME: (7%, 200%, 7%, 700%, 0mm, -100mm [rcr])
+    molMoveRel(out, 1473.12f, 41666f, 1473.12f, 145833f, bboxWidth*208.33, 0f); // FIXME: (7%, 200%, 7%, 700%, 100mm, 0mm [rcr])
+    molMoveRel(out, 1473.12f, 41666f, 1041f, 145833f, 0f, bboxHeight*208.33); // FIXME: (7%, 200%, 5%, 700%, 0mm, 100mm [rcr])
   // 05f0: End Of Motion Block (started at 0430, 112 words)
     molCmdEndMotionBlock(out);
     molCmdEndSubroutine(out, 1);
@@ -673,10 +748,11 @@ public class MPC6515Cutter extends LaserCutter
     molCmdBeginMotionBlock(out);
     molMoveRel(out, 1041f, 5208f, 1014.73f, 104166f, 417f, 417f); // (5%, 25%, 5%, 500%, 2.00163mm, 2.00163mm [rcr])
     molCmdLaserOn(out); // 06d8
-    molMoveRel(out, 1014.73f, 5208f, 1473.12f, 104166f, (-bboxWidth-4.0)*208.33, 0f); // (5%, 25%, 7%, 500%, -104.003mm, 0mm [rcr])
-    molMoveRel(out, 1473.12f, 5208f, 1473.12f, 104166f, 0f, (-bboxHeight-4.0)*208.33); // (7%, 25%, 7%, 500%, 0mm, -104.003mm [rcr])
-    molMoveRel(out, 1473.12f, 5208f, 1473.12f, 104166f, (bboxWidth+4.0)*208.33, 0f); // (7%, 25%, 7%, 500%, 104.003mm, 0mm [rcr])
-    molMoveRel(out, 1473.12f, 5208f, 1041f, 104166f, 0f, (bboxHeight+4.0)*208.33); // (7%, 25%, 5%, 500%, 0mm, 104.003mm [rcr])
+    // TODO: verify that the correct origin is used, verify if correct after flipping coordinates
+    molMoveRel(out, 1014.73f, 5208f, 1473.12f, 104166f, (-bboxWidth-4.0)*208.33, 0f); // FIXME: (5%, 25%, 7%, 500%, -104.003mm, 0mm [rcr])
+    molMoveRel(out, 1473.12f, 5208f, 1473.12f, 104166f, 0f, (-bboxHeight-4.0)*208.33); // FIXME: (7%, 25%, 7%, 500%, 0mm, -104.003mm [rcr])
+    molMoveRel(out, 1473.12f, 5208f, 1473.12f, 104166f, (bboxWidth+4.0)*208.33, 0f); // FIXME: (7%, 25%, 7%, 500%, 104.003mm, 0mm [rcr])
+    molMoveRel(out, 1473.12f, 5208f, 1041f, 104166f, 0f, (bboxHeight+4.0)*208.33); // FIXME: (7%, 25%, 5%, 500%, 0mm, 104.003mm [rcr])
     molCmdLaserOff(out); // 08a0
   // 08a8: End Of Motion Block (started at 0668, 144 words)
     molCmdEndMotionBlock(out);
@@ -694,6 +770,9 @@ public class MPC6515Cutter extends LaserCutter
   public void sendJob(LaserJob job, ProgressListener pl, List<String> warnings) throws IllegalJobException, Exception
   {
     // This is a first try in Java. All data is saved into ~/RUN.MOL until we figure out the USB communication
+    currentPower = 40.0;
+    currentSpeed = 80.0;
+    motionBlockStart = -1;
     
     pl.taskChanged(this, "checking...");
     checkJob(job);
@@ -702,6 +781,7 @@ public class MPC6515Cutter extends LaserCutter
     firstVector = true;
     for (JobPart p : job.getParts())
     {
+      // FIXME: use job.getMinX(), job.getMaxX(), etc.
       if (p instanceof VectorPart)
       {
         double resolution = p.getDPI();
@@ -713,7 +793,7 @@ public class MPC6515Cutter extends LaserCutter
             case MOVETO:
             {
               double dx, x = Util.px2mm(c.getX(), resolution);
-              double dy, y = Util.px2mm(c.getY(), resolution);
+              double dy, y = this.bedHeight - Util.px2mm(c.getY(), resolution);
               if (firstVector) {
                 firstVector = false;
                 bboxMinX = bboxMaxX = firstX = prevX = x;
@@ -759,7 +839,6 @@ public class MPC6515Cutter extends LaserCutter
     
     molWriteBlock0600(out);
 
-
   // 
   // .MOL file data block at 0x00000a00
   // 
@@ -782,32 +861,12 @@ public class MPC6515Cutter extends LaserCutter
   // 0a30: Unknown Command 15
     molWriteBytes(out, 0x41, 0x4b, 0x00, 0x01);
     molWriteInt(out, 515);
-  // 0a38: Laser Power
-    molWriteBytes(out, 0x46, 0x0e, 0x00, 0x05);
-    molWriteInt(out, 4000);  // (40.000000%) corner power
-    molWriteInt(out, 4000);  // (40.000000%) maximum power
-    molWriteFloat(out, 1041f); // (5%) cutting start speed
-    molWriteFloat(out, 20833f); // (100%) maximum cutting speed
-    molWriteInt(out, 0);  // unknown
-  // 0a50: Laser Power Long
-    molWriteBytes(out, 0x46, 0x0e, 0x00, 0x07);
-    molWriteInt(out, 4000);  // (40.000000%) corner power
-    molWriteInt(out, 4000);  // (40.000000%) maximum power
-    molWriteInt(out, 3000);  // (30.000000%) ?
-    molWriteInt(out, 5000);  // (50.000000%) ?
-    molWriteFloat(out, 1041f); // (5%) cutting start speed
-    molWriteFloat(out, 20833f); // (100%) maximum cutting speed
-    molWriteInt(out, 0);  // unknown
-  // 0a70: Begin Motion Block
-    molCmdBeginMotionBlock(out);
-    molCmdBlowerOn(out);
-
+  // first vector will set the speed and start the motion block!
+    
     // generate the commands to draw the artwork
     firstVector = true;
-    int bailCnt = 9999999; // FIXME: no longer used?! 10, 100, 1000(F), 500(F), 300(F), 200, 250, 275, 290, 295(F), 293(F), 292(F), 291(OK)
     for (JobPart p : job.getParts())
     {
-      if (bailCnt<=0) break;
       if (p instanceof RasterPart)
       {
         throw new IllegalJobException("The MPC6515 currently does not support engraving");
@@ -821,21 +880,27 @@ public class MPC6515Cutter extends LaserCutter
         double resolution = p.getDPI();
         for (VectorCommand c : ((VectorPart) p).getCommandList())
         {
-          if (bailCnt<=0) break;
           switch (c.getType())
           {
             case LINETO:
             case MOVETO:
             {
-              // TODO: Motion Block must be *less* than 512 words (just start a new block)!
-              bailCnt--;
               double dx, x = Util.px2mm(c.getX(), resolution);
-              double dy, y = Util.px2mm(c.getY(), resolution);
+              double dy, y = this.bedHeight - Util.px2mm(c.getY(), resolution);
+              // FIXME: job.getStartX() job.getStartY(), what do they do? Will that help us here?
               if (firstVector) {
                 firstVector = false;
+                // 0a38: Laser Power
+                Debug.println("Setting power and speed");
+                molSetPowerAndSpeed(out, currentPower*cornerPowerFactor, currentPower, 5.0, currentSpeed);
+                // 0a70: Begin Motion Block
+                molCmdBeginMotionBlock(out);
+                molCmdBlowerOn(out);
+                // TODO: what exactly is the origin at this point?
                 firstX = prevX = x;
                 firstY = prevY = y;
                 if (firstX!=bboxMinX || firstY!=bboxMinY) {
+                  // TODO: if this is a LINETO, move to the origin, then cut to the coordinate
                   molMoveRel(out, 936.9f, 20833f, 936.9f, 145833f, (firstX-bboxMinX)*208.33, (firstY-bboxMinY)*208.33);
                 }
               } else {
@@ -844,7 +909,7 @@ public class MPC6515Cutter extends LaserCutter
                 if (dx!=0.0 || dy!=0.0) {
                   if (c.getType()==VectorCommand.CmdType.LINETO) {
                     molCmdLaserOn(out);
-                    molMoveRel(out, 936.9f, 25.0*208.33, 936.9f, 104166f, dx*208.33, dy*208.33);
+                    molMoveRel(out, 936.9f, currentSpeed*208.33, 936.9f, 104166f, dx*208.33, dy*208.33);
                     molCmdLaserOff(out);
                   } else {
                     molMoveRel(out, 936.9f, 35406.7f, 936.9f, 145833f, dx*208.33, dy*208.33);
@@ -855,22 +920,30 @@ public class MPC6515Cutter extends LaserCutter
             }
             case SETPROPERTY:
             {
-              //TODO: WRITE IN MOL FORMAT
-              //PowerSpeedFocusFrequencyProperty prop = (PowerSpeedFocusFrequencyProperty) c.getProperty();
-              //out.write(10);
+              PowerSpeedFocusFrequencyProperty prop = (PowerSpeedFocusFrequencyProperty) c.getProperty();
+              currentPower = prop.getPower(); //power in 0-100
+              currentSpeed = prop.getSpeed(); //speed in 0-100
+              Debug.println("Set Power to " + Double.toString(currentPower) + "  Speed to " + Double.toString(currentSpeed));
               //out.write(prop.getPower());//power in 0-100
               //out.write(prop.getSpeed());//speed in 0-100
               //out.write((int) prop.getFocus());//focus in mm
               //out.write(prop.getFrequency());//frequency in Hz
+              if (!firstVector) {
+                Debug.println("Changing power and speed");
+                molChangePowerAndSpeed(out, currentPower*cornerPowerFactor, currentPower, 5.0, currentSpeed);
+              }
               break;
             }
           }
         }
       }
     }
-    molCmdLaserOff(out);
-    molCmdBlowerOff(out);
-    molCmdEndMotionBlock(out);
+    if (molInMotionBlock()) {
+      // don't write this blcok if no vectors were sent at all...
+      molCmdLaserOff(out);
+      molCmdBlowerOff(out);
+      molCmdEndMotionBlock(out);
+    }
     molCmdEndSubroutine(out, 3);
 
     molFixBlock0600(out);
@@ -929,6 +1002,10 @@ public class MPC6515Cutter extends LaserCutter
     {
       setBedHeight((Double) value);
     }
+    else if (CORNER_POWER_FACTOR.equals(key))
+    {
+      cornerPowerFactor = (Double)value;
+    }
     else
     {
       System.err.println("ERROR: Unknown property '"+key+"' for MPC6151 driver");
@@ -945,6 +1022,10 @@ public class MPC6515Cutter extends LaserCutter
     else if (BED_HEIGHT.equals(key))
     {
       return getBedHeight();
+    }
+    else if (CORNER_POWER_FACTOR.equals(key))
+    {
+      return cornerPowerFactor;
     }
     else
     {
