0fea943449797013c5cc59e61fd0d4d1cb6c133e
Max Gaukler <development@maxgaukler.de>
2016-08-03 14:52:02 +0200

    Copyright header check: Allow other authors

--- test.sh
+++ test.sh
@@ -8,10 +8,12 @@ then
 	exit 1
 fi
 HEADERSIZE=$(wc -l < copyrightheader)
+# filter out the line that contains year, author name(s) and at least one email adress enclosed in <>
+IGNORE_AUTHOR_LINE_REGEXP='^ \* Copyright \(C\) 20[0-9]{2}.*<.*@.*>.*$'
 ERRORS=0
 for f in $(find src -name '*.java')
 do
-	if ! cat $f|head -n $HEADERSIZE|diff - copyrightheader > /dev/null
+	if ! diff <(cat $f | head -n $HEADERSIZE | egrep -v "$IGNORE_AUTHOR_LINE_REGEXP") <(cat copyrightheader | egrep -v "$IGNORE_AUTHOR_LINE_REGEXP")
 	then
 		echo "Copyright header mismatch on $f"
 		ERRORS=1
