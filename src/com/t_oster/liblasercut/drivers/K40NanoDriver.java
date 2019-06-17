9ad2ad269dba26ca48746a961a7dd5231df09a6c
tatarize <tatarize@gmail.com>
2019-06-17 06:17:26 -0700

    Fixed extra space in copyright notice.

--- src/com/t_oster/liblasercut/drivers/K40NanoDriver.java
+++ src/com/t_oster/liblasercut/drivers/K40NanoDriver.java
@@ -15,8 +15,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with LibLaserCut. If not, see <http://www.gnu.org/licenses/>.
  *
- *
- */
+ **/
+
+
 package com.t_oster.liblasercut.drivers;
 
 import com.t_oster.liblasercut.IllegalJobException;
