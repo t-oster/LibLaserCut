fbc16015ed2fc804106085a8cc9eeecf93d43b66
tatarize <tatarize@gmail.com>
2019-06-19 14:39:48 -0700

    Header Correction

--- src/com/t_oster/liblasercut/Raster3dPart.java
+++ src/com/t_oster/liblasercut/Raster3dPart.java
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with LibLaserCut. If not, see <http://www.gnu.org/licenses/>.
  *
- *
- */
+ **/
+
 package com.t_oster.liblasercut;
 
 import com.t_oster.liblasercut.platform.Point;
