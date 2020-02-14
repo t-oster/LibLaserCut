39f21eb74b44b3272fd93f6c5e9df55682695de1
Max Gaukler <development@maxgaukler.de>
2020-02-14 20:11:47 +0100

    fixup previous commit

--- src/main/java/de/thomas_oster/liblasercut/BlackWhiteRaster.java
+++ src/main/java/de/thomas_oster/liblasercut/BlackWhiteRaster.java
@@ -236,13 +236,12 @@ public class BlackWhiteRaster extends TimeIntensiveOperation implements Greyscal
   @Override
   public int getGreyScale(int x, int y)
   {
-    return getPixel(x, y) * 255;
+    return isBlack(x,y) ? 0 : 255;
   }
 
   @Override
   public void setGreyScale(int x, int y, int grey)
   {
-    setPixel(x, y, grey > 127 ? 1 : 0);
+    setBlack(x, y, grey < 127);
   }
-
 }
