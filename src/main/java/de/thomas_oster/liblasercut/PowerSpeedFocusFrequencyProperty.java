5286f27c07f8ee03581d500ffd954f22c668ff31
Philipp Dörfler <ph@doerfler.io>
2020-04-12 19:07:40 +0200

    Fixed dangling Javadoc comments (#151)
    * Fixed dangling Javadoc comments
    
    Having Javadoc at the beginning of a .java file is an error. I converted these into block comments.
    
    * Adapted copyright check

--- src/main/java/de/thomas_oster/liblasercut/PowerSpeedFocusFrequencyProperty.java
+++ src/main/java/de/thomas_oster/liblasercut/PowerSpeedFocusFrequencyProperty.java
@@ -1,21 +1,21 @@
-/**
- * This file is part of LibLaserCut.
- * Copyright (C) 2011 - 2014 Thomas Oster <mail@thomas-oster.de>
- *
- * LibLaserCut is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * LibLaserCut is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with LibLaserCut. If not, see <http://www.gnu.org/licenses/>.
- *
- **/
+/*
+  This file is part of LibLaserCut.
+  Copyright (C) 2011 - 2014 Thomas Oster <mail@thomas-oster.de>
+
+  LibLaserCut is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  LibLaserCut is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with LibLaserCut. If not, see <http://www.gnu.org/licenses/>.
+
+ */
 package de.thomas_oster.liblasercut;
 
 /**
