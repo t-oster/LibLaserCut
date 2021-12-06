89f976d19191b946937c0df0a65c44165ce4728a
Timo <timo.birnschein@microforge.de>
2021-12-05 20:43:51 -0800

    Update src/main/java/de/thomas_oster/liblasercut/drivers/GenericGcodeDriver.java
    Tested and works as intended when Force Laser Of During G0 Moves is checked in the laser cuter gcode driver.
    
    Co-authored-by: Max Gaukler <mgmax@users.noreply.github.com>

--- src/main/java/de/thomas_oster/liblasercut/drivers/GenericGcodeDriver.java
+++ src/main/java/de/thomas_oster/liblasercut/drivers/GenericGcodeDriver.java
@@ -507,7 +507,7 @@ public class GenericGcodeDriver extends LaserCutter {
        String append = "";
        if (blankLaserDuringRapids) {
           append = " S0";
-          currentPower = 0.0;
+          currentPower = -1; // set to invalid value to force new S-value at next G1
        }
        sendLine("G0 Z%s" + append, df.format(focus));
     }
